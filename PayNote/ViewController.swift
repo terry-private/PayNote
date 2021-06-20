6fd0cc4754d5d9ebdfe50d578a901bd05160de6a
teruhito <ataeteruhito@gmail.com>
2021-06-20 17:10:54 +0900

    Initial Commit

---
+++ PayNote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PayNote
+//
+//  Created by 若江照仁 on 2021/06/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
